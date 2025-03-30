9217a2fe7aaf20d124ca4af1e0b05a483c434714
Stanislav Karkavin <me@xdefrag.dev>
2025-03-30 18:16:03 +0200

    feat: add redirects

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/ru/tokens/stas",
+        destination: "/stas",
+        permanent: true,
+      },
+      {
+        source: "/ru/verification/mtla",
+        destination: "/verify",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
